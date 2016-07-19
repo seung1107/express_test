48332b3f87ec5e8b7f77b46d90a824503809dc15
Johnny Kim <seung6870@gmail.com>
2016-07-19 23:04:41 +0900

    no message

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
  
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,6 +29,12 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
  
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true
+}));
+
 app.use('/', routes);
 app.use('/users', users);
  
@@ -37,6 +44,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
  
 // error handlers
  
--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,24 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', function(req, res, next){
+	  if(req.session.logined)
+	    res.render('logout', {session: req.session})
+	  else {
+	    res.render('login', {session: req.session})
+	  }
+	 
+	})
+
+router.post('/login', function(req, res, next){
+	  if(req.body.id == 'dalkom' & req.body.pw =='itworld')
+	  req.session.regenerate(function(){
+	    req.session.logined = true;
+	    req.session.user_id = req.body.id;
+	 
+	    res.render('logout', {session: req.session})
+	  })
+	});
  
 module.exports = router;
