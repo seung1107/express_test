d173bd63a5d096fbb950515db83436878c84b921
Johnny Kim <seung6870@gmail.com>
2016-07-20 23:14:36 +0900

    no message

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,14 @@ router.get('/dor', function(req, res, next){
 	res.send('ㅅㅂㄻ');
 });
 
+router.get('/login', function(req, res, next){
+	  if(req.session.logined)
+	    res.render('logout', {session: req.session})
+	  else {
+	    res.render('login', {session: req.session})
+	  }
+	 
+	})
+
+
 module.exports = router;
